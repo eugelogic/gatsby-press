0e23fb3f27162e64647e0a3f86cdddb866251428
Eugene Molari <eugelogic@gmail.com>
2020-04-06 12:26:02 +0100

    Add and config gatsby-source-graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,15 @@ module.exports = {
     title: `Headless WordPress with Gatsby`,
     description: `Demo project of a headless WordPress site with Gatsby on the front end.`,
     author: `@EugeneMolari`
-  }
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `WPGQL`,
+        fieldName: `wpgql`,
+        url: `https://gatsbypress.website/graphql`
+      }
+    }
+  ]
 }
