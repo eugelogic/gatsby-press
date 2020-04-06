7a26473b48490669c1ee9e9b74d949d794dba8f9
Eugene Molari <eugelogic@gmail.com>
2020-04-06 12:08:58 +0100

    Add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Headless WordPress with Gatsby`,
+    description: `Demo project of a headless WordPress site with Gatsby on the front end.`,
+    author: `@EugeneMolari`
+  }
 }
