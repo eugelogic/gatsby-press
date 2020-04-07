0e56ab58970f33c26a8b9e52f9bd6d01fd8528d1
Eugene Molari <eugelogic@gmail.com>
2020-04-07 22:24:00 +0100

    Capitalize all components files

--- src/templates/user.js
+++ src/templates/user.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 
 const UserTemplate = props => {
     const { data: { wpgql: { user } } } = props
