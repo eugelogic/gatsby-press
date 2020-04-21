c13062b7bce984b1f2035bd89f2085201e4ffd0c
Eugene Molari <eugelogic@gmail.com>
2020-04-21 15:12:10 +0100

    Edit and style footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,8 @@
 import React from 'react'
 
 const Footer = () => (
-    <footer>
-        © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://wordpress.org/">WordPress</a>
-          {` and `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <footer className="footer">
+        <p>© {new Date().getFullYear()}, Built with <a href="https://www.gatsbyjs.org">Gatsby</a> and <a href="https://wordpress.org/">WordPress</a></p>
     </footer>
 )
 
